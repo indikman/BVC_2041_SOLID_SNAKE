c51a4d95d3ee357ff9a750b605f8483340792cd6
Christian Rojas <koenigrojass@gmail.com>
2024-06-17 15:46:07 -0600

    Task 3 working on
    -Still need to show the task

---
+++ Assets/Student_Assets/ChristianRojasOrtiz_Assts/Scripts/TaskManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Task
+{
+    public string TaskName { get; set; }
+    public bool IsComplete { get; set; }
+}
+public class TaskManager : MonoBehaviour
+{
+    [SerializeField] private List<Task> Tasks = new List<Task>();
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip taskCompleteSound;
+
+    private bool hasPlayedSound = false;
+
+    private void Update()
+    {
+        if (AreAllTasksComplete() && !hasPlayedSound)
+        {
+            audioSource.PlayOneShot(taskCompleteSound);
+            hasPlayedSound = true;
+        }
+        DebugTasks();
+    }
+   
+   private bool AreAllTasksComplete()
+   {
+       return Tasks.All(task => task.IsComplete);
+   }
+   
+   public void AddTask(string taskName)
+   {
+       Task newTask = new Task { TaskName = taskName, IsComplete = false };
+       Tasks.Add(newTask);
+   }
+   
+   public void CompleteTask(string taskName)
+   {
+       Task taskToComplete = Tasks.FirstOrDefault(task => task.TaskName == taskName);
+       if (taskToComplete != null)
+       {
+           taskToComplete.IsComplete = true;
+       }
+   }
+   
+   public void DebugTasks()
+   {
+       foreach (Task task in Tasks)
+       {
+           Debug.Log($"Task: {task.TaskName}, Is Complete: {task.IsComplete}");
+       }
+   }
+}
