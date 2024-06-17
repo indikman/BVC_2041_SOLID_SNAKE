c97d4771959a76bcd086fdde41b74c5a6353d7a0
Garan <g.schulz505@mybvc.ca>
2024-06-17 00:19:01 -0600

    Progress on Task 1

--- Assets/Student_Assets/GaranSchulz/Scripts/Managers/TaskManager.cs
+++ Assets/Student_Assets/GaranSchulz/Scripts/Managers/TaskManager.cs
@@ -7,14 +7,18 @@ using UnityEngine.Events;
 public class TaskManager : MonoBehaviour
 {
     [SerializeField] private TaskSO[] tasks;
-
+    
     private void Awake()
     {
-        tasks[0].IsComplete += TestFunction;
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            int index = i;
+            tasks[index].IsComplete += () => TestFunction(index);
+        }
     }
 
-    public void TestFunction()
+    public void TestFunction(int number)
     {
-        Debug.Log("does it work?");
+        Debug.Log("does it work?" + number);
     }
 }
