75df67366ae1862bb5aee049d4e49c58a407d359
Clara Gavinho <clara.gavinho@gmail.com>
2024-07-20 12:58:33 -0600

    Door closing

--- Assets/Student_Assets/Clara/Scripts/Door.cs
+++ Assets/Student_Assets/Clara/Scripts/Door.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public bool isOpening = false;
-    public GameObject door;
+    [SerializeField] private float duration = 2.0f;
+    [SerializeField] private float angle = 90.0f;
+    [SerializeField] private float waitTime = 2f;
+    [SerializeField] Transform doorPivot;
 
-    private void Update()
+    public bool isOpening = false;
+    public bool isClosing = false;
+
+    private void Start()
     {
-        if (Input.GetKeyDown("h"))
-            OpenDoor();
+        if (doorPivot != null) 
+        {
+            doorPivot = transform;
+        }
     }
     public void OpenDoor()
     {
-        if (!isOpening)
+        if (!isOpening && !isClosing) 
         {
-            door.transform.Rotate(0, 90, 0);
-            isOpening = true;
+            StartCoroutine(DoorMove());
         }
     }
-    public void CloseDoor()
+    private IEnumerator DoorMove()
     {
-        door.transform.Rotate(0, 0, 0);
+        isOpening = true;
+        Quaternion firstRotation = doorPivot.rotation;
+        Quaternion rotationMove = Quaternion.Euler(doorPivot.eulerAngles + new Vector3(0, angle, 0));
+
+        for (float i = 0; i < duration; i += Time.deltaTime)
+        {
+            doorPivot.rotation = Quaternion.Slerp(firstRotation, rotationMove, i/duration);
+            yield return null;
+        }
         isOpening = false;
+
+        yield return new WaitForSeconds(waitTime);
+        isClosing = true;
+
+        for (float i = 0; i < duration; i += Time.deltaTime)
+        {
+            doorPivot.rotation = Quaternion.Slerp(rotationMove, firstRotation, i / duration);
+            yield return null;
+        }
+
+        isClosing = false;
     }
 }
