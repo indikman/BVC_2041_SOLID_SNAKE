25be101ee118659a76b22f5ebb7babcc31f69c77
CaCollinson64 <146494565+CaCollinson64@users.noreply.github.com>
2024-07-25 20:25:43 -0600

    Added vars, Assistance from AngelusNein/Ryan

--- Assets/Student_Assets/DoorRotation.cs
+++ Assets/Student_Assets/DoorRotation.cs
@@ -1,11 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using DG.Tweening;
+using Vector3 = UnityEngine.Vector3;
 
 public class DoorRotation : MonoBehaviour
 {
+    [SerializeField] private Vector3 openedPosition;
+
+    [SerializeField] private Vector3 closedPosition;
+
+    [SerializeField] private float doorSpeed = 10f;
+
+    [SerializeField] private Vector3 startingPosition;
+
+    [SerializeField] private bool isOpened;
     // Start is called before the first frame update
+    // Thank you to Ryan / AngelusNein for assistance.
+    // using comments to show understanding and not just copypasting lol
+    
+    private void Awake()
+    {
+        //On awake, the door sets its current position into a variable
+        startingPosition = transform.position;
+    }
+
     void Start()
     {
         
@@ -19,6 +40,7 @@ public class DoorRotation : MonoBehaviour
 
     public void DoorRotate()
     {
-        transform.DORotate(new Vector3(-90, 90, 0), 2f, RotateMode.Fast);
+        // Door opens by taking the vars and pumping them into DOrotate from DOTWEEN
+        transform.DORotate(new Vector3(-90, 90, 0), 10f, RotateMode.Fast);
     }
 }
