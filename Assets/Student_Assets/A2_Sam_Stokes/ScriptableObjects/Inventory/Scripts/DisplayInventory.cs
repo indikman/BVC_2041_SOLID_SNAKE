f026ed6ccc92ad3f304cf3dc89b7ed16c82b135e
Sam stokes <s.stokes954@mybvc.ca>
2024-07-26 10:00:09 -0600

    Task 1 Complete
    Items are collectable and show in players inventory and the UI inventory.

--- Assets/Student_Assets/A2_Sam_Stokes/ScriptableObjects/Inventory/Scripts/DisplayInventory.cs
+++ Assets/Student_Assets/A2_Sam_Stokes/ScriptableObjects/Inventory/Scripts/DisplayInventory.cs
@@ -33,7 +33,15 @@ public class DisplayInventory : MonoBehaviour
             {
                 itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
             }
+            else
+            {
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
         }
+
     }
     public void CreateDisplay()
     {
@@ -42,6 +50,7 @@ public class DisplayInventory : MonoBehaviour
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            itemsDisplayed.Add(inventory.Container[i], obj);
         }
     }
     public Vector3 GetPosition(int i)
