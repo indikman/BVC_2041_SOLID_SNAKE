fe5842472af561564ba13cdcd023b5d09f890f92
ayden <theownage79@gmail.com>
2024-08-07 22:49:08 -0600

    use Pointer interface to show description

--- Assets/Student_Assets/Ayden's/Scripts/the new system/InventoryButton.cs
+++ Assets/Student_Assets/Ayden's/Scripts/the new system/InventoryButton.cs
@@ -4,28 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class InventoryButton : MonoBehaviour
+public class InventoryButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Image image;
     private GameObject _GameObject;
     public ItemSO _itemSo;
     private Button _button;
     private Transform _transform;
+    private TMP_Text _text;
+    [SerializeField] private TMP_Text descriptionText;
 
     private void Awake()
     {
         _button = GetComponent<Button>();
         image = GetComponent<Image>();
+        _text = GetComponentInChildren<TMP_Text>();
         _transform = FindObjectOfType<hands>().transform;
         image.sprite = _itemSo._sprite;
         _GameObject = _itemSo._gameObject;
+        _text.text = _itemSo.ItemName;
+        descriptionText.text = _itemSo.ItemDescription;
+        descriptionText.enabled = false;
     }
 
     private void Start()
     {
         _button.onClick.AddListener(ItemSet);
+        
     }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        descriptionText.enabled = true;
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        descriptionText.enabled = false;
+    }
+    
+    
 
     private void ItemSet()
     {
