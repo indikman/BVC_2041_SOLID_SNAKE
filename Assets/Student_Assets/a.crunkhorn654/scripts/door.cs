e6d38fbaf0f1df2afa75486f87202cb7d67df5cd
acrunkhorn <a.crunkhorn@proton.me>
2024-08-11 21:49:07 -0600

    inventory slots responding

--- Assets/Student_Assets/a.crunkhorn654/scripts/door.cs
+++ Assets/Student_Assets/a.crunkhorn654/scripts/door.cs
@@ -9,38 +9,54 @@ public class door : MonoBehaviour
     Quaternion targetRotation;
     float rotateTime = 1.0f;
 
+    public void Awake()
+    {
+        targetRotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+    }
 
     public void cycleDoor()
     {
+            
         Debug.Log("door cycle");
         if (doorOpen)
         {
+            doorOpen = false;
             CloseEvent();
         }
         else
         {
+            doorOpen = true;
             OpenEvent();
         }
+            
     }
 
 
     public void OpenEvent()
     {
+        /*
         doorRotation = transform.rotation;
-        targetRotation = new Quaternion(transform.rotation.x, .7f, transform.rotation.z, transform.rotation.w);
+        targetRotation.Set(transform.rotation.x + 1, transform.rotation.y, transform.rotation.z, transform.rotation.w);
         StartCoroutine(Open());
+        */
+
+        gameObject.transform.Rotate(0f, 0f, 90f);
     }
 
     public void CloseEvent()
     {
+        /*
         doorRotation = transform.rotation;
-        targetRotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, transform.rotation.w);
+        targetRotation.Set(transform.rotation.x - 1, transform.rotation.y, transform.rotation.z, transform.rotation.w);
         StartCoroutine(Open());
         Debug.Log("close event");
+        */
+        gameObject.transform.Rotate(0f, 0f, -90f);
     }
 
     IEnumerator Open()
     {
+        
         float i = 0;
         while (i < 1)
         {
@@ -49,6 +65,7 @@ public class door : MonoBehaviour
             transform.rotation = doorRotation;
             yield return null;
         }
+        
         transform.rotation = targetRotation;
         yield return null;
     }
